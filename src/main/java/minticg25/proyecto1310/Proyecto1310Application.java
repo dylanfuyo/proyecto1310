1b1973a156039c59646475297907cc1040b6c04d
BRAYAND YESID FUYO GONZALEZ <brayand.fuyo.mt@correo.usa.edu.co>
2021-10-14 19:34:15 -0500

    proyecto Springboot

---
+++ src/main/java/minticg25/proyecto1310/Proyecto1310Application.java
@@ -0,0 +1,13 @@
+package minticg25.proyecto1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1310Application.class, args);
+	}
+
+}
---
+++ src/test/java/minticg25/proyecto1310/Proyecto1310ApplicationTests.java
@@ -0,0 +1,13 @@
+package minticg25.proyecto1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
